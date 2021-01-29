7f1c4d2d98575d677d715d3c9479c2a52094947a
pomoranca <armin_x@hotmail.com>
2021-01-29 16:03:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/galleryapplication/ImageViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.galleryapplication
+
+import android.database.Cursor
+import android.net.Uri
+import android.provider.MediaStore
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * Use Coroutines To Load Images
+ */
+class ImageViewModel : ViewModel() {
+
+
+}
